ad61d712af4f5835c94d9b3fd23c6b1e382cbb4a
Novacki <novackifernando@hotmail.com>
2023-02-09 23:17:58 -0300

    create startup class

---
+++ src/Server/Recruiters.API/Settings/Startup/Interfaces/IStartupSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Recruiters.API.Settings.Startup.Interfaces
+{
+    public interface IStartupSettings
+    {
+        public IStartupSettings AddConfiguration(IConfiguration configuration);
+        public IConfiguration Configuration { get; }
+        public IStartupSettings ConfigureServices(IServiceCollection services);
+        public void Configure(WebApplication app);
+    }
+}
